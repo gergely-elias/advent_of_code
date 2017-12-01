59ab3c36882e7094c41a992256370c2370771b49
Gergely Elias <gergely.elias@gmail.com>
2017-12-01 16:19:45 +0100

    problem 1 of 2017

---
+++ 2017/d01c2.py
@@ -0,0 +1,11 @@
+input_file = open('inputd01.txt','r')
+input_lines = input_file.readlines()
+
+word = input_lines[0].strip()
+sum_of_digits = 0
+comparing_offset = len(word) / 2
+for i in range(len(word)):
+  if word[i] == word[(i + comparing_offset) % len(word)]:
+    sum_of_digits += int(word[i])
+print(sum_of_digits)
+
