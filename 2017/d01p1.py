e6cfe0fe50c79675953437f72f3125d6e2cef561
Gergely Elias <gergely.elias@gmail.com>
2017-12-02 06:56:24 +0100

    improvements based on suggestions from @narimiran

--- 2017/d01p1.py
+++ 2017/d01p1.py
@@ -1,11 +1,11 @@
 input_file = open('inputd01.txt','r')
 input_lines = input_file.readlines()
 
-word = input_lines[0].strip()
+digit_sequence = input_lines[0].strip()
 sum_of_digits = 0
 comparing_offset = 1
-for i in range(len(word)):
-  if word[i] == word[(i + comparing_offset) % len(word)]:
-    sum_of_digits += int(word[i])
+for i,x in enumerate(digit_sequence):
+  if x == digit_sequence[(i + comparing_offset) % len(digit_sequence)]:
+    sum_of_digits += int(x)
 print(sum_of_digits)
 
--- 2017/d02p2.py
+++ 2017/d02p2.py
@@ -6,10 +6,10 @@ for line in input_lines:
   numbers_on_line = [int(x) for x in line.strip().split('\t')]
   line_length = len(numbers_on_line)
   numbers_on_line_found = False
-  for i in range(line_length):
-    for j in range(line_length):
-      if i != j and numbers_on_line[i] % numbers_on_line[j] == 0:
-        checksum += numbers_on_line[i]/numbers_on_line[j]
+  for i1,x1 in enumerate(numbers_on_line):
+    for i2,x2 in enumerate(numbers_on_line):
+      if i1 != i2 and x1 % x2 == 0:
+        checksum += x1 / x2
         numbers_on_line_found = True
         break
     if numbers_on_line_found:
