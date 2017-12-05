1219bd3ea5b901585d7f82771fed49d514f12ea4
Gergely Elias <gergely.elias@gmail.com>
2017-12-05 06:32:45 +0100

    problem 5 of 2017

---
+++ 2017/d05p1.py
@@ -0,0 +1,19 @@
+input_file = open('inputd05.txt','r')
+input_lines = input_file.readlines()
+
+message = []
+offset = 0
+number_of_jumps = 0
+
+for line in input_lines:
+  message.append(int(line.strip()))
+
+while True:
+  jump = message[offset]
+  message[offset] += 1
+  offset += jump
+  number_of_jumps += 1
+  if offset < 0 or offset >= len(message):
+    break
+print number_of_jumps
+
