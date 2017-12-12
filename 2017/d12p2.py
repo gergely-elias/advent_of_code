4bc05536d6665d919aed795f7cb2001558e7c7a3
Gergely Elias <gergely.elias@gmail.com>
2017-12-12 13:27:22 +0100

    problem 12 of 2017

---
+++ 2017/d12p2.py
@@ -0,0 +1,29 @@
+input_file = open('inputd12.txt','r')
+input_lines = input_file.readlines()
+
+import re
+
+neighbours = []
+for line in input_lines:
+  neighbours.append([int(x) for x in re.findall('\d+', line.strip())][1:])
+  
+group = set()
+remaining_ids = set(range(len(neighbours)))
+connected = set()
+number_of_groups = 0
+
+while len(remaining_ids) > 0:
+  connected = set([list(remaining_ids)[0]])
+  while len(connected) > 0:
+    program_id = list(connected)[0]
+    connected.remove(program_id)
+    remaining_ids.remove(program_id)
+    group.add(program_id)
+    for neighbour in neighbours[program_id]:
+      if neighbour not in group and neighbour not in connected:
+        connected.add(neighbour)
+  number_of_groups += 1
+
+print number_of_groups 
+
+
