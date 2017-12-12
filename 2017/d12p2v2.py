6af3383b93d19423a09ed805b7946dc8cfb277e7
Gergely Elias <gergely.elias@gmail.com>
2017-12-12 14:42:59 +0100

    alternative solution for problem 12 of 2017

---
+++ 2017/d12p2v2.py
@@ -0,0 +1,19 @@
+input_file = open('inputd12.txt','r')
+input_lines = input_file.readlines()
+
+import re
+
+groups = []
+
+for line in input_lines:
+  program_ids = set([int(x) for x in re.findall('\d+', line.strip())])
+  for i,group in enumerate(groups):
+    if len(program_ids.intersection(group)) > 0:
+      program_ids.update(group)
+      groups.pop(i)
+  groups.append(program_ids)
+
+print len(groups)
+    
+  
+
