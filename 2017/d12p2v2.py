e44b433a7a933645524179711efacbc9c06a0a51
Gergely Elias <gergely.elias@gmail.com>
2017-12-14 12:21:11 +0100

    bug correction: undesired side effects of popping while traversing the same sequence

--- 2017/d12p2v2.py
+++ 2017/d12p2v2.py
@@ -7,7 +7,7 @@ groups = []
 
 for line in input_lines:
   program_ids = set([int(x) for x in re.findall('\d+', line.strip())])
-  for i,group in enumerate(groups):
+  for i,group in reversed(list(enumerate(groups))):
     if len(program_ids.intersection(group)) > 0:
       program_ids.update(group)
       groups.pop(i)
