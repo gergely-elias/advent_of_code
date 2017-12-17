171490f0d4f3ad377c93e21a5ba2dcb672a9e9d5
Gergely Elias <gergely.elias@gmail.com>
2017-12-17 09:11:18 +0100

    problem 17 of 2017

---
+++ 2017/d17p1.py
@@ -0,0 +1,11 @@
+input_file = open('inputd17.txt','r')
+input_lines = input_file.readlines()
+
+steps = int(input_lines[0].strip())
+buffer_state = [0]
+current_position = 0
+
+for value_to_insert in range(1, 2018):
+  current_position = (current_position + steps) % len(buffer_state) + 1
+  buffer_state = buffer_state[:current_position] + [value_to_insert] + buffer_state[current_position:]
+print buffer_state[current_position + 1]
---
+++ 2017/d17p2.py
@@ -0,0 +1,14 @@
+input_file = open('inputd17.txt','r')
+input_lines = input_file.readlines()
+
+steps = int(input_lines[0].strip())
+buffer_length = 1
+current_position = 0
+value_after_zero = None
+
+for value_to_insert in range(1, 50000001):
+  current_position = (current_position + steps) % buffer_length + 1
+  buffer_length += 1
+  if current_position == 1:
+    value_after_zero = value_to_insert
+print value_after_zero
