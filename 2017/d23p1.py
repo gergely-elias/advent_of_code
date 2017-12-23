ad49f4b833ce88f8647d8d05db41a35218b0acbe
Gergely Elias <gergely.elias@gmail.com>
2017-12-23 17:17:32 +0100

    problem 23 of 2017

---
+++ 2017/d23p1.py
@@ -0,0 +1,31 @@
+input_file = open('inputd23.txt','r')
+input_lines = input_file.readlines()
+
+import re
+import collections
+
+instructions = [line.strip().split() for line in input_lines]
+registers = collections.defaultdict(lambda: 0)
+instruction_index = 0
+number_of_mul_instructions = 0
+
+def substitute(value):
+  if re.match('-?\d+', value):
+    return int(value)
+  return registers[value]
+
+while instruction_index in range(0, len(instructions)):
+  instruction = instructions[instruction_index]
+  if instruction[0] == 'set':
+    registers[instruction[1]] = substitute(instruction[2])
+  elif instruction[0] == 'sub':
+    registers[instruction[1]] -= substitute(instruction[2])
+  elif instruction[0] == 'mul':
+    registers[instruction[1]] *= substitute(instruction[2])
+    number_of_mul_instructions += 1
+  elif instruction[0] == 'jnz' and substitute(instruction[1]) != 0:
+    instruction_index += substitute(instruction[2])
+    continue
+  instruction_index += 1
+
+print number_of_mul_instructions
---
+++ 2017/d23p2.py
@@ -0,0 +1,20 @@
+input_file = open('inputd23.txt','r')
+input_lines = input_file.readlines()
+
+range_start = int(input_lines[0].strip().split()[-1]) \
+            * int(input_lines[4].strip().split()[-1]) \
+            - int(input_lines[5].strip().split()[-1])
+range_length = - int(input_lines[7].strip().split()[-1])
+range_step = - int(input_lines[-2].strip().split()[-1])
+
+search_range = range(range_start, range_start + range_length + 1, range_step)
+composite_numbers = 0
+for number in search_range:
+  is_prime = True
+  for possible_divisor in range(2,int(number ** 0.5 + 1)):
+    if number % possible_divisor == 0:
+      is_prime = False
+      break
+  if is_prime == False:
+    composite_numbers += 1
+print composite_numbers
