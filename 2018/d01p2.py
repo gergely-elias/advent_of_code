18dc3ed492b5b2bf52eb2d5406565b673744c696
Gergely Elias <gergely.elias@gmail.com>
2018-12-01 15:22:04 +0100

    bug correction: original freq of 0 considered seen to pass given test cases

--- 2018/d01p2.py
+++ 2018/d01p2.py
@@ -2,7 +2,7 @@ input_file = open('inputd01.txt','r')
 input_lines = input_file.readlines()
 
 freq = 0
-former_freqs = set()
+former_freqs = set([0])
 while True:
   for line in input_lines:
     line = int(line.strip())
