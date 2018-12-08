529d4fe361edc34e056eb8824b15c6e044f6c1a3
Gergely Elias <gergely.elias@gmail.com>
2018-12-08 07:24:39 +0100

    problem 8 of 2018

---
+++ 2018/d08p1.py
@@ -0,0 +1,26 @@
+input_file = open('inputd08.txt','r')
+input_lines = input_file.readlines()
+
+import re
+
+tree = map(int, re.findall('\d+', input_lines[0].strip()))
+
+index = 0
+numbers_of_children = []
+metadata_lengths = []
+result_sum = 0
+
+def parse():
+  global index, numbers_of_children, metadata_lengths, result_sum
+  numbers_of_children.append(tree[index])
+  metadata_lengths.append(tree[index + 1])
+  index += 2
+  for child in range(numbers_of_children[-1]):
+    parse()
+  numbers_of_children.pop()
+  result_sum += sum(tree[index:index + metadata_lengths[-1]])
+  index += metadata_lengths[-1]
+  metadata_lengths.pop()
+
+parse()
+print result_sum
---
+++ 2018/d08p2.py
@@ -0,0 +1,42 @@
+input_file = open('inputd08.txt','r')
+input_lines = input_file.readlines()
+
+import re
+import sys
+
+sys.setrecursionlimit(5000)
+
+tree = map(int, re.findall('\d+', input_lines[0].strip()))
+
+index = 0
+numbers_of_children = []
+metadata_lengths = []
+metadata_stack = []
+last_metadata_sum = 0
+
+def parse():
+  global index, numbers_of_children, metadata_lengths, metadata_stack, last_metadata_sum
+  numbers_of_children.append(tree[index])
+  metadata_lengths.append(tree[index + 1])
+  index += 2
+  
+  for child in range(numbers_of_children[-1]):
+    parse()
+
+  if numbers_of_children[-1] == 0:
+    metadata_sum = sum(tree[index:index + metadata_lengths[-1]])
+  else:
+    metadata_sum = 0
+    for metadata_reference in [x - 1 for x in tree[index:index + metadata_lengths[-1]]]:
+      if metadata_reference < len(metadata_stack[-numbers_of_children[-1]:]):
+        metadata_sum += metadata_stack[-numbers_of_children[-1]:][metadata_reference]
+    del metadata_stack[-numbers_of_children[-1]:]
+  metadata_stack.append(metadata_sum)
+  index += metadata_lengths[-1]
+
+  numbers_of_children.pop()
+  metadata_lengths.pop()
+  last_metadata_sum = metadata_stack[-1]
+
+parse()
+print last_metadata_sum
