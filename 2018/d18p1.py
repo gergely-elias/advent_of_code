d0542c720160b7dafc4455e0f92b89e61bb44754
Gergely Elias <gergely.elias@gmail.com>
2018-12-18 10:14:28 +0100

    problem 18 of 2018

---
+++ 2018/d18p1.py
@@ -0,0 +1,31 @@
+input_file = open('inputd18.txt','r')
+input_lines = input_file.readlines()
+
+import copy
+import collections
+
+landscape = collections.defaultdict(lambda: -1)
+area_size = len(input_lines)
+
+for y in range(area_size):
+  line = input_lines[y].strip()
+  for x in range(area_size):
+    landscape[y, x] = ['.', '|', '#'].index(line[x])
+
+target_minute = 10
+current_minute = 0
+while current_minute < target_minute:
+  next_landscape = copy.deepcopy(landscape)
+  for y in range(area_size):
+    for x in range(area_size):
+      neighbourhood = [landscape[neighbour_y, neighbour_x] for neighbour_x in range(x - 1, x + 2) for neighbour_y in range(y - 1, y + 2)]
+      if landscape[y, x] == 0 and neighbourhood.count(1) >= 3:
+        next_landscape[y, x] = 1
+      elif landscape[y, x] == 1 and neighbourhood.count(2) >= 3:
+        next_landscape[y, x] = 2
+      elif landscape[y, x] == 2 and (neighbourhood.count(2) < 2 or neighbourhood.count(1) < 1):
+        next_landscape[y, x] = 0
+  landscape = next_landscape
+  current_minute += 1
+
+print landscape.values().count(1) * landscape.values().count(2)
