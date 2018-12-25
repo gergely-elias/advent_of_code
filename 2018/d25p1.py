0480fb84680818ed56d6f42608024a88cce0f11c
Gergely Elias <gergely.elias@gmail.com>
2018-12-25 06:35:57 +0100

    problem 25 of 2018

---
+++ 2018/d25p1.py
@@ -0,0 +1,22 @@
+input_file = open('inputd25.txt','r')
+input_lines = input_file.readlines()
+
+import itertools
+import re
+import networkx
+
+def dist(pair):
+  return sum([abs(coord1 - coord2) for coord1, coord2 in zip(*pair)])
+
+constellation = networkx.Graph()
+points = []
+for line in input_lines:
+  point = tuple(map(int,re.findall('-?\d+', line.strip())))
+  points.append(point)
+  constellation.add_node(point)
+
+for pair in itertools.combinations(points, 2):
+  if dist(pair) <= 3:
+    constellation.add_edge(*pair)
+
+print networkx.number_connected_components(constellation)
