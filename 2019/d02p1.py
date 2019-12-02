2a8e091fa96b0731df3571cd01a89aca3f6f34b7
Gergely Elias <gergely.elias@vacuumlabs.com>
2019-12-02 06:43:17 +0100

    problem 2 of 2019

---
+++ 2019/d02p1.py
@@ -0,0 +1,22 @@
+input_file = open('inputd02.txt','r')
+input_lines = input_file.readlines()
+
+program = list(map(int, input_lines[0].split(',')))
+program[1] = 12
+program[2] = 2
+
+opcode_length = 4
+for position in range(0, len(program), opcode_length):
+  opcode = program[position : position + opcode_length]
+  operation = opcode[0]
+  input1 = program[opcode[1]]
+  input2 = program[opcode[2]]
+  if operation == 1:
+    result = input1 + input2
+  elif operation == 2:
+    result = input1 * input2
+  else:
+    break
+  program[opcode[3]] = result
+
+print(program[0])
