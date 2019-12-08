43fc006d29f72eab9cedc5b052338b195fee9af1
Gergely Elias <gergely.elias@vacuumlabs.com>
2019-12-08 20:36:52 +0100

    problem 8 of 2019

---
+++ 2019/d08p1.py
@@ -0,0 +1,19 @@
+input_file = open('inputd08.txt','r')
+input_lines = input_file.readlines()
+
+import collections
+
+image = input_lines[0].strip()
+height = 6
+width = 25
+layer_size = height * width
+
+min_zeroes = float("inf")
+result = 0
+for layer_starting_pixel in range(0, len(image), layer_size):
+  layer = image[layer_starting_pixel : layer_starting_pixel + layer_size]
+  digit_frequency = collections.Counter(layer)
+  if digit_frequency['0'] < min_zeroes:
+    min_zeroes = digit_frequency['0']
+    result = digit_frequency['1'] * digit_frequency['2']
+print(result)
---
+++ 2019/d08p2.py
@@ -0,0 +1,15 @@
+input_file = open('inputd08.txt','r')
+input_lines = input_file.readlines()
+
+image = input_lines[0].strip()
+height = 6
+width = 25
+layer_size = height * width
+
+result = ['?'] * layer_size
+for layer_starting_pixel in range(0, len(image), layer_size):
+  layer = image[layer_starting_pixel : layer_starting_pixel + layer_size]
+  result = [' X?'[int(layer[pixel_index])] if result[pixel_index] == '?' else result[pixel_index] for pixel_index in range(layer_size)]
+
+for row_index in range(height):
+  print(''.join(result[row_index * width : (row_index + 1) * width]))
