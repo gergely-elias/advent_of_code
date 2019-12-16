04aed22fd22cdd0d55dafcf684244c8a75620a9d
Gergely Elias <gergely.elias@vacuumlabs.com>
2019-12-16 12:13:39 +0100

    problem 16 of 2019

---
+++ 2019/d16p2.py
@@ -0,0 +1,21 @@
+input_file = open('inputd16.txt','r')
+input_lines = input_file.readlines()
+
+import re
+
+signal = list(map(int, list(re.findall('\d', input_lines[0]))))
+signal *= 10000
+offset = int(''.join(map(str, signal[:7])))
+assert(offset >= len(signal) // 2)
+signal = signal[offset:]
+
+n = len(signal)
+for iteration in range(100):
+  new_signal = [None] * n
+  tail_sum = 0
+  for signal_index in range(n):
+    tail_sum += signal[n - 1 - signal_index]
+    new_signal[n - 1 - signal_index] = abs(tail_sum) % 10
+  signal = new_signal[:]
+
+print(''.join(map(str,signal[:8])))
