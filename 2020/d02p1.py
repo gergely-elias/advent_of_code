2bfacea307a8ef945d87709b5cb2d6357eb0aee6
Gergely Elias <gergely.elias@gmail.com>
2020-12-02 08:57:33 +0100

    problem 2 of 2020

---
+++ 2020/d02p1.py
@@ -0,0 +1,16 @@
+input_file = open('inputd02.txt','r')
+input_lines = input_file.readlines()
+
+import re
+
+def parse_line(line):
+  fields = re.split("\W+", line)
+  return {
+    'min_count': int(fields[0]),
+    'max_count': int(fields[1]),
+    'letter': fields[2],
+    'password': fields[3],
+  }
+
+passwords_with_policies = [parse_line(line) for line in input_lines]
+print(sum([entry['password'].count(entry['letter']) in range(entry['min_count'], entry['max_count'] + 1) for entry in passwords_with_policies]))
