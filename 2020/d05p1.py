97db3d470d4e4fd9f4847f347c99e2fc4f416ff7
Gergely Elias <gergely.elias@gmail.com>
2020-12-05 06:35:38 +0100

    problem 5 of 2020

---
+++ 2020/d05p1.py
@@ -0,0 +1,5 @@
+input_file = open('inputd05.txt','r')
+input_lines = input_file.readlines()
+
+ids = [int(''.join(['1' if s in 'RB' else '0' for s in line.strip()]), 2) for line in input_lines]
+print(max(ids))
