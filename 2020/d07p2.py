63c9e30354d6b4280c76516cc3e867970fba8b1f
Gergely Elias <gergely.elias@gmail.com>
2020-12-09 16:02:03 +0100

    optimization for day 7 part 2 of 2020, skipping calculation for nodes that are not descendants of target

--- 2020/d07p2.py
+++ 2020/d07p2.py
@@ -17,14 +17,20 @@ for line in input_lines:
             weight=(0 if inner_bag[0] == "" else int(inner_bag[0])),
         )
 
+target_bag = "shiny gold bag"
 number_of_bags_inside = dict()
-for bag in reversed(list(networkx.topological_sort(bags))):
+
+bags_inside_target_bag = [
+    bag
+    for bag in reversed(list(networkx.topological_sort(bags)))
+    if bag in networkx.descendants(bags, target_bag).union([target_bag])
+]
+
+for bag in bags_inside_target_bag:
     bags_inside_current_bag = 0
     for outer_bag, inner_bag, edge_data in bags.out_edges(bag, data=True):
         bags_inside_current_bag += edge_data["weight"] * (
             1 + number_of_bags_inside[inner_bag]
         )
-    if bag == "shiny gold bag":
-        print(bags_inside_current_bag)
-        exit()
     number_of_bags_inside[bag] = bags_inside_current_bag
+print(number_of_bags_inside[target_bag])
