7c9e421ef0081e9cb74ef0a53bf2d2119e10672c
Gergely Elias <gergely.elias@gmail.com>
2020-12-18 07:01:33 +0100

    problem 18 of 2020

---
+++ 2020/d18p2.py
@@ -0,0 +1,27 @@
+import fileinput
+import re
+
+input_lines = list(fileinput.input())
+
+
+def expression_value(expression_match):
+    parsed_expression = expression_match.group()[1:-1].split()
+    value = int(parsed_expression[0])
+    for term_index in range(1, len(parsed_expression), 2):
+        if parsed_expression[term_index] == "+":
+            value += int(parsed_expression[term_index + 1])
+        elif parsed_expression[term_index] == "*":
+            value *= int(parsed_expression[term_index + 1])
+        else:
+            assert False
+    return str(value)
+
+
+sum_of_lines = 0
+for line in input_lines:
+    reduced_line = "(" + line.strip() + ")"
+    while "(" in reduced_line:
+        reduced_line = re.sub("(\d+(?:\ \+\ \d+)+)", r"(\1)", reduced_line)
+        reduced_line = re.sub("\([^\(\)]+\)", expression_value, reduced_line)
+    sum_of_lines += int(reduced_line)
+print(sum_of_lines)
