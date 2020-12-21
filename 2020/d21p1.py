e64b30d88fee2b57a1f7bb92e8491b7e90354652
Gergely Elias <gergely.elias@gmail.com>
2020-12-21 06:53:21 +0100

    problem 21 of 2020

---
+++ 2020/d21p1.py
@@ -0,0 +1,42 @@
+import fileinput
+import networkx
+import re
+import collections
+
+input_lines = list(fileinput.input())
+
+possibly_allergen_ingredient = dict()
+all_ingredients = collections.defaultdict(lambda: 0)
+for line in input_lines:
+    ingredient_block, allergen_block = re.findall(
+        "(.*)\ \(contains\ (.*)\)", line.strip()
+    )[0]
+    ingredients = ingredient_block.split(" ")
+    allergens = allergen_block.split(", ")
+    for allergen in allergens:
+        if allergen not in possibly_allergen_ingredient:
+            possibly_allergen_ingredient[allergen] = set(ingredients)
+        else:
+            possibly_allergen_ingredient[allergen] = possibly_allergen_ingredient[
+                allergen
+            ] & set(ingredients)
+    for ingredient in ingredients:
+        all_ingredients[ingredient] += 1
+
+allergen_ingredient_possibilities = networkx.Graph()
+for allergen, ingredients in possibly_allergen_ingredient.items():
+    allergen_ingredient_possibilities.add_edges_from(
+        [(allergen, ingredient) for ingredient in ingredients]
+    )
+allergen_ingredients = networkx.bipartite.maximum_matching(
+    allergen_ingredient_possibilities
+)
+
+print(
+    sum(
+        [
+            0 if ingredient in allergen_ingredients else ingredient_count
+            for ingredient, ingredient_count in all_ingredients.items()
+        ]
+    )
+)
