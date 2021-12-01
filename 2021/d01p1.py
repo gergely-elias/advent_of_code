90e7e5c0781913224322fceeda1f2dc10136b39b
Gergely Elias <gergely.elias@gmail.com>
2021-12-01 06:26:02 +0100

    problem 1 of 2021

---
+++ 2021/d01p1.py
@@ -0,0 +1,14 @@
+import fileinput
+
+input_lines = list(fileinput.input())
+
+depths = list(map(int, input_lines))
+
+print(
+    sum(
+        [
+            next_depth > prev_depth
+            for next_depth, prev_depth in zip(depths[1:], depths[:-1])
+        ]
+    )
+)
