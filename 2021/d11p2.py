f5ab41a36b9f758dd8394fc2fbaabc8d439b4a92
Gergely Elias <gergely.elias@gmail.com>
2021-12-11 10:46:46 +0100

    problem 11 of 2021

---
+++ 2021/d11p2.py
@@ -0,0 +1,51 @@
+import fileinput
+
+input_lines = list(fileinput.input())
+
+number_of_rows = len(input_lines)
+number_of_columns = len(input_lines[0].strip())
+
+octopis_energies = {
+    (y, x): int(input_lines[y][x])
+    for y in range(number_of_rows)
+    for x in range(number_of_columns)
+}
+neighbours_offsets = [
+    (neighbour_y_offset, neighbour_x_offset)
+    for neighbour_y_offset in range(-1, 1 + 1)
+    for neighbour_x_offset in range(-1, 1 + 1)
+    if any([coord != 0 for coord in (neighbour_y_offset, neighbour_x_offset)])
+]
+octopi_flashing = {coords: False for coords in octopis_energies.keys()}
+max_non_flashing_energy = 9
+
+steps = 0
+while sum(octopi_flashing.values()) < len(octopi_flashing):
+    steps += 1
+    for coords in octopis_energies:
+        if octopi_flashing[coords]:
+            octopi_flashing[coords] = False
+            octopis_energies[coords] = 0
+
+    for coords in octopis_energies:
+        octopis_energies[coords] += 1
+
+    check_new_flash = True
+    while check_new_flash:
+        check_new_flash = False
+        for coords in octopis_energies:
+            if octopis_energies[coords] > max_non_flashing_energy:
+                if not octopi_flashing[coords]:
+                    for neighbour_coord in [
+                        tuple(
+                            sum(component_coord)
+                            for component_coord in list(zip(coords, neighbour_offset))
+                        )
+                        for neighbour_offset in neighbours_offsets
+                    ]:
+                        if neighbour_coord in octopis_energies:
+                            octopis_energies[neighbour_coord] += 1
+                    octopi_flashing[coords] = True
+                    check_new_flash = True
+
+print(steps)
