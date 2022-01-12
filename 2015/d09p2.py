3cc3d014e04671c2d9133059acf15041c8ae3d61
Gergely Elias <gergely.elias@gmail.com>
2022-01-12 08:00:32 +0100

    generalisation (no assumption on input size) for day 9 of 2015

--- 2015/d09p2.py
+++ 2015/d09p2.py
@@ -3,20 +3,21 @@ import itertools
 
 input_lines = list(fileinput.input())
 
-distances = 8 * [0]
+number_of_locations = int((len(input_lines) * 2) ** 0.5) + 1
+distances = number_of_locations * [0]
 index = 0
-for i in range(8):
-    distances[i] = 8 * [0]
+for i in range(number_of_locations):
+    distances[i] = number_of_locations * [0]
 
-for i in range(8 - 1):
-    for j in range(i + 1, 8):
+for i in range(number_of_locations - 1):
+    for j in range(i + 1, number_of_locations):
         dist = int(input_lines[index].strip().split(" ")[-1])
         distances[i][j] = dist
         distances[j][i] = dist
         index += 1
 
 max_total_distance = 0
-all_city_permutations = itertools.permutations(range(8))
+all_city_permutations = itertools.permutations(range(number_of_locations))
 for city_order in all_city_permutations:
     current_total_distance = 0
     for i in range(len(city_order) - 1):
