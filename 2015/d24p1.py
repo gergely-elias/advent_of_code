a90008e6d62226b8e4174fde84c57a12942f8c44
Gergely Elias <gergely.elias@gmail.com>
2022-02-04 16:41:51 +0100

    typo correction, extracting magic constant

--- 2015/d24p1.py
+++ 2015/d24p1.py
@@ -6,22 +6,23 @@ input_lines = list(fileinput.input())
 
 package_weights = [int(line) for line in input_lines]
 
-target_weight = sum(package_weights) // 3
-possible_passenger_comparments = []
-for number_of_packages in range(1, len(package_weights) // 3 + 1):
+number_of_compartments = 3
+target_weight = sum(package_weights) // number_of_compartments
+possible_passenger_compartments = []
+for number_of_packages in range(1, len(package_weights) // number_of_compartments + 1):
     for passenger_compartment in itertools.combinations(
         package_weights, number_of_packages
     ):
         if sum(passenger_compartment) == target_weight:
-            possible_passenger_comparments.append(passenger_compartment)
-    if len(possible_passenger_comparments) > 0:
+            possible_passenger_compartments.append(passenger_compartment)
+    if len(possible_passenger_compartments) > 0:
         break
 
 print(
     min(
         [
             math.prod(passenger_compartment)
-            for passenger_compartment in possible_passenger_comparments
+            for passenger_compartment in possible_passenger_compartments
         ]
     )
 )
