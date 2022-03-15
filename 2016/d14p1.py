abd03e50f20bea9505e150e5d9f4ce7ac89c67ab
Gergely Elias <gergely.elias@gmail.com>
2022-03-15 21:44:53 +0100

    bug correction: first occurence of triplet and quintuplet might be of different characters

--- 2016/d14p1.py
+++ 2016/d14p1.py
@@ -14,17 +14,19 @@ candidates = []
 limit = None
 while limit is None or index <= limit:
     hashresult = hashlib.md5((salt + str(index)).encode("utf-8")).hexdigest()
-    match = re.search(r"(\w)\1{2}", hashresult)
-    if match is not None:
+    match3 = re.search(r"(\w)\1{2}", hashresult)
+    if match3 is not None:
+        digit3 = match3.group()[0]
         while len(candidates) > 0 and candidates[0][0] < index - interval:
             candidates.pop(0)
-        digit = match.group()[0]
-        if re.search(digit * 5, hashresult) is not None:
+        match5 = re.search(r"(\w)\1{4}", hashresult)
+        if match5 is not None:
+            digit5 = match5.group()[0]
             for earlier_candidate_index, earlier_candidate_digit in candidates:
-                if earlier_candidate_digit == digit:
+                if earlier_candidate_digit == digit5:
                     keys_produced.add(earlier_candidate_index)
                     if len(keys_produced) >= keys_to_produce and limit is None:
                         limit = index + interval
-        candidates.append((index, digit))
+        candidates.append((index, digit3))
     index += 1
 print(sorted(list(keys_produced))[keys_to_produce - 1])
