b89059834701a914c9fcee1749f5148b8ead03fd
Gergely Elias <gergely.elias@gmail.com>
2022-12-12 06:41:03 +0100

    problem 12 of 2022

---
+++ 2022/d12p1.py
@@ -0,0 +1,39 @@
+import fileinput
+import networkx
+import string
+
+input_lines = list(fileinput.input())
+
+start_location = None
+end_location = None
+elevation = []
+for y, line in enumerate(input_lines):
+    row = []
+    for x, char in enumerate(line.strip()):
+        if char in string.ascii_lowercase:
+            row.append(string.ascii_lowercase.index(char))
+        elif char == "S":
+            start_location = (y, x)
+            row.append(0)
+        elif char == "E":
+            end_location = (y, x)
+            row.append(len(string.ascii_lowercase) - 1)
+    elevation.append(row)
+
+elevationmapgraph = networkx.DiGraph()
+for y in range(len(elevation)):
+    for x in range(len(elevation[0])):
+        if y > 0 and elevation[y][x] - elevation[y - 1][x] <= 1:
+            elevationmapgraph.add_edge((y - 1, x), (y, x))
+        if y > 0 and elevation[y - 1][x] - elevation[y][x] <= 1:
+            elevationmapgraph.add_edge((y, x), (y - 1, x))
+        if x > 0 and elevation[y][x] - elevation[y][x - 1] <= 1:
+            elevationmapgraph.add_edge((y, x - 1), (y, x))
+        if x > 0 and elevation[y][x - 1] - elevation[y][x] <= 1:
+            elevationmapgraph.add_edge((y, x), (y, x - 1))
+
+print(
+    networkx.shortest_path_length(
+        elevationmapgraph, source=start_location, target=end_location
+    )
+)
