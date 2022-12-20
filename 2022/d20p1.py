fede0d59d87737f7ec408df05afa4e1b381eb2d8
Gergely Elias <gergely.elias@gmail.com>
2022-12-20 07:24:01 +0100

    problem 20 of 2022

---
+++ 2022/d20p1.py
@@ -0,0 +1,33 @@
+import fileinput
+
+input_lines = list(fileinput.input())
+enumerated_coordinates = list(enumerate([int(line.strip()) for line in input_lines]))
+number_of_coordinates = len(enumerated_coordinates)
+
+for original_position_to_move in range(number_of_coordinates):
+    for current_position, (original_position, coordinate) in enumerate(
+        enumerated_coordinates
+    ):
+        if original_position == original_position_to_move:
+            break
+    enumerated_coordinates.pop(current_position)
+    next_position = (current_position + coordinate) % (number_of_coordinates - 1)
+    enumerated_coordinates = (
+        enumerated_coordinates[:next_position]
+        + [(original_position, coordinate)]
+        + enumerated_coordinates[next_position:]
+    )
+
+for current_position, (original_position, coordinate) in enumerate(
+    enumerated_coordinates
+):
+    if coordinate == 0:
+        position_of_zero = current_position
+        break
+
+print(
+    sum(
+        enumerated_coordinates[(offset + position_of_zero) % number_of_coordinates][1]
+        for offset in [1000, 2000, 3000]
+    )
+)
