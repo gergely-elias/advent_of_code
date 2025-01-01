86c68fe3d54c3f6c8e9057cc37fb2f87a7212237
Gergely Elias <gergely.elias@gmail.com>
2025-01-02 00:02:53 +0100

    optimization for day 11 part 2 of 2024, memorizing number transformations

--- 2024/d11p2.py
+++ 2024/d11p2.py
@@ -6,17 +6,22 @@ numbers = map(int, input_lines[0].strip().split())
 blinks = 75
 
 number_tally = collections.Counter(numbers)
+mapping = {0: [1]}
 for blink in range(blinks):
     new_number_tally = collections.defaultdict(int)
     for n in number_tally:
-        n_as_string = str(n)
-        digits = len(n_as_string)
-        if n == 0:
-            new_number_tally[1] += number_tally[n]
-        elif digits % 2 == 0:
-            new_number_tally[int(n_as_string[: digits // 2])] += number_tally[n]
-            new_number_tally[int(n_as_string[digits // 2 :])] += number_tally[n]
-        else:
-            new_number_tally[2024 * n] += number_tally[n]
+        if n not in mapping:
+            n_as_string = str(n)
+            digits = len(n_as_string)
+            if digits % 2 == 0:
+                mapping[n] = [
+                    int(n_as_string[: digits // 2]),
+                    int(n_as_string[digits // 2 :]),
+                ]
+            else:
+                mapping[n] = [2024 * n]
+        for mapped_n in mapping[n]:
+            new_number_tally[mapped_n] += number_tally[n]
+            new_number_tally[mapped_n] %= 10**25 + 7
     number_tally = new_number_tally
 print(sum(number_tally.values()))
